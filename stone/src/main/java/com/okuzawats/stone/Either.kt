a61699382169a30f15749fbb6d67bd34bbeb657c
okuzawats <okuzawats@gmail.com>
2021-06-21 08:43:32 +0900

    make bounds of E to throwable

--- stone/src/main/java/com/okuzawats/stone/Either.kt
+++ stone/src/main/java/com/okuzawats/stone/Either.kt
@@ -1,6 +1,6 @@
 package com.okuzawats.stone
 
-sealed class Either<E, T> {
-  data class Left<E>(val error: E) : Either<E, Nothing>()
+sealed class Either<out E : Throwable, T> {
+  data class Left<out E : Throwable>(val error: E) : Either<E, Nothing>()
   data class Right<T>(val value: T) : Either<Nothing, T>()
 }
--- stone/src/test/java/com/okuzawats/stone/EitherTest.kt
+++ stone/src/test/java/com/okuzawats/stone/EitherTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 class EitherTest {
   @Test
   fun testEither() {
-    Either.Left(error = Throwable())
+    Either.Left(error = Throwable("oops!"))
     Either.Right(value = 42)
   }
 }

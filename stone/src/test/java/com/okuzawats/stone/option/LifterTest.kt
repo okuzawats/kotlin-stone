52d05b7b9b14283a7541a5eedab03a32fe58345b
okuzawats <okuzawats@gmail.com>
2021-06-22 06:14:02 +0900

    write Linter test

--- stone/src/test/java/com/okuzawats/stone/option/LifterTest.kt
+++ stone/src/test/java/com/okuzawats/stone/option/LifterTest.kt
@@ -1,5 +1,21 @@
 package com.okuzawats.stone.option
 
+import org.junit.Test
+
 class LifterTest {
-  
+  @Test
+  fun testToOption_fromNonNull() {
+    val int: Int? = 42
+    val actual = int.toOption()
+
+    assert(actual is Option.Some)
+  }
+
+  @Test
+  fun testToOption_fromNullable() {
+    val int: Int? = null
+    val actual = int.toOption()
+
+    assert(actual is Option.None)
+  }
 }

0783b22b730b244ca0cee4259c26ac39d29fd37c
okuzawats <okuzawats@gmail.com>
2021-06-22 05:49:01 +0900

    write KDoc for Option

--- stone/src/main/java/com/okuzawats/stone/Option.kt
+++ stone/src/main/java/com/okuzawats/stone/Option.kt
@@ -1,6 +1,25 @@
 package com.okuzawats.stone
 
+import com.okuzawats.stone.Option.None
+import com.okuzawats.stone.Option.Some
+
+/**
+ * Has [Some] or [None].
+ *
+ * [Some] has a value lf [T].
+ * [None] do not have any value.
+ */
 sealed class Option<T> {
+
+  /**
+   * A subclass of [Option] and has a value of [T].
+   *
+   * @param value [T]
+   */
   data class Some<T>(val value: T) : Option<T>()
+
+  /**
+   * A subclass of [Option] and do not have any value.
+   */
   object None : Option<Nothing>()
 }

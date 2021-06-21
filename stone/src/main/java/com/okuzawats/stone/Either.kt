f7d390f26cd630383b7130596563ef79b93549c8
okuzawats <okuzawats@gmail.com>
2021-06-22 05:44:19 +0900

    write KDoc of Either

--- stone/src/main/java/com/okuzawats/stone/Either.kt
+++ stone/src/main/java/com/okuzawats/stone/Either.kt
@@ -1,6 +1,24 @@
 package com.okuzawats.stone
 
+/**
+ * Has [Left] or [Right].
+ *
+ * [Left] has a [Throwable].
+ * [Right] has a value of [T].
+ */
 sealed class Either<out E : Throwable, T> {
+
+  /**
+   * A subclass of [Either] and has an error.
+   *
+   * @param error Throwable
+   */
   data class Left<out E : Throwable>(val error: E) : Either<E, Nothing>()
+
+  /**
+   * A subclass of [Either] and has a value of [T].
+   *
+   * @param value [T]
+   */
   data class Right<T>(val value: T) : Either<Nothing, T>()
 }

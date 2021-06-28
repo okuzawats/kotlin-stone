e5b5012928519cccb62953bdf404d656351fbba3
okuzawats <okuzawats@gmail.com>
2021-06-29 05:46:04 +0900

    reformat OptionExtensions

--- stone/src/main/java/com/okuzawats/stone/OptionExtensions.kt
+++ stone/src/main/java/com/okuzawats/stone/OptionExtensions.kt
@@ -5,10 +5,11 @@ package com.okuzawats.stone
  *
  * @return [T] the value from Some or default
  */
-fun <T : Any> Option<T>.getOrElse(default: T): T = when (this) {
-  is Option.Some -> value
-  is Option.None -> default
-}
+fun <T : Any> Option<T>.getOrElse(default: T): T =
+  when (this) {
+    is Option.Some -> value
+    is Option.None -> default
+  }
 
 /**
  * transform Option<T> to Option<S>
@@ -18,10 +19,11 @@ fun <T : Any> Option<T>.getOrElse(default: T): T = when (this) {
  * @param transform transformer from [T] to [S]
  * @return Option<S>. if it is None, return None
  */
-fun <T : Any, S : Any> Option<T>.map(transform: (T) -> S): Option<S> = when (this) {
-  is Option.Some -> Option.Some(value = transform(value))
-  is Option.None -> Option.None
-}
+fun <T : Any, S : Any> Option<T>.map(transform: (T) -> S): Option<S> =
+  when (this) {
+    is Option.Some -> Option.Some(value = transform(value))
+    is Option.None -> Option.None
+  }
 
 /**
  * transform Option<T> to Option<S>
@@ -31,10 +33,11 @@ fun <T : Any, S : Any> Option<T>.map(transform: (T) -> S): Option<S> = when (thi
  * @param transform transformer from [T] to Option[S]
  * @return Option<S>. if it is None, return None
  */
-fun <T : Any, S : Any> Option<T>.flatMap(transform: (T) -> Option<S>): Option<S> = when (this) {
-  is Option.Some -> transform(value)
-  is Option.None -> Option.None
-}
+fun <T : Any, S : Any> Option<T>.flatMap(transform: (T) -> Option<S>): Option<S> =
+  when (this) {
+    is Option.Some -> transform(value)
+    is Option.None -> Option.None
+  }
 
 /**
  * return Some<T> if it is Some and predicate is true, else None
@@ -44,11 +47,14 @@ fun <T : Any, S : Any> Option<T>.flatMap(transform: (T) -> Option<S>): Option<S>
  * @param predicate predicate applied to [T]
  * @return Option<T> filtered by the [predicate]
  */
-fun <T : Any> Option<T>.filter(predicate: (T) -> Boolean): Option<T> = when (this) {
-  is Option.Some -> if (predicate(value)) {
-    Option.Some(value = value)
-  } else {
-    Option.None
+fun <T : Any> Option<T>.filter(predicate: (T) -> Boolean): Option<T> =
+  when (this) {
+    is Option.Some -> if (predicate(value)) {
+      Option.Some(value = value)
+    } else {
+      Option.None
+    }
+    is Option.None -> {
+      Option.None
+    }
   }
-  is Option.None -> Option.None
-}
